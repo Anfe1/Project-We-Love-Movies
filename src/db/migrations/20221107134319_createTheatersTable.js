ce8d9377d1cd6c42aba3e86fba2eed0144e4be00
Andy Fernandez <andy.w.fernandez@gmail.com>
2022-11-07 17:12:30 -0700

    modified moviesTheaters and theaters migration files.

--- src/db/migrations/20221107134319_createTheatersTable.js
+++ src/db/migrations/20221107134319_createTheatersTable.js
@@ -5,7 +5,6 @@ exports.up = function (knex) {
     table.string("address_line_1");
     table.string("address_line_2");
     table.string("city");
-    table.string("city");
     table.string("state");
     table.string("zip");
     table.timestamp(true, true);
