908c2d06e79f6697eaa8495a7ae8ac43dcacf2c0
Andy Fernandez <andy.w.fernandez@gmail.com>
2022-11-09 16:12:04 -0700

    Added all the logic to get all theaters listed

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const cors = require("cors");
 
 const moviesRouter = require("./movies/movies.router");
 const reviewsRouter = require("./reviews/reviews.router");
+const theatersRouter = require("./theaters/theaters.router");
 const errorHandler = require("./errors/errorHandler");
 const notFound = require("./errors/notFound");
 // const logger = require("./config/logger");
@@ -17,6 +18,8 @@ app.use(express.json());
 //----router----//
 app.use("/movies", moviesRouter);
 app.use("/reviews", reviewsRouter);
+app.use("/theaters", theatersRouter);
+
 //----Error handlers----//
 app.use(notFound);
 app.use(errorHandler);
